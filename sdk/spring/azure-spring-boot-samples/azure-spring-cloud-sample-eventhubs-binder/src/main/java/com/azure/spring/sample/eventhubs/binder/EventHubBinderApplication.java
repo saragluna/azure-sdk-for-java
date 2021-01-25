c4d8b1abe7f5bd4150b9da665fbfca61b902de34
Xiaolu Dai <xiada@microsoft.com>
2021-01-25 08:53:38 +0800

    using functional programming model

--- sdk/spring/azure-spring-boot-samples/azure-spring-cloud-sample-eventhubs-binder/src/main/java/com/azure/spring/sample/eventhubs/binder/EventHubBinderApplication.java
+++ sdk/spring/azure-spring-boot-samples/azure-spring-cloud-sample-eventhubs-binder/src/main/java/com/azure/spring/sample/eventhubs/binder/EventHubBinderApplication.java
@@ -3,8 +3,17 @@
 
 package com.azure.spring.sample.eventhubs.binder;
 
+import com.azure.spring.integration.core.api.reactor.Checkpointer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+
+import java.util.function.Consumer;
+
+import static com.azure.spring.integration.core.AzureHeaders.CHECKPOINTER;
 
 /**
  * @author Warren Zhu
@@ -12,7 +21,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventHubBinderApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(EventHubBinderApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(EventHubBinderApplication.class, args);
     }
+
+    @Bean
+    public Consumer<Message<String>> consume() {
+        return message -> {
+            Checkpointer checkpointer = (Checkpointer) message.getHeaders().get(CHECKPOINTER);
+            LOGGER.info("New message received: '{}'", message);
+            checkpointer.success()
+                        .doOnSuccess(success -> LOGGER.info("Message '{}' successfully checkpointed", message))
+                        .doOnError(error -> LOGGER.error("Exception: {}", error.getMessage()))
+                        .subscribe();
+        };
+    }
+
 }
